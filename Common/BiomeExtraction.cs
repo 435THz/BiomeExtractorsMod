ee58af41b141b98e077531017ef959f2f46ca939
MistressNebula <delbovemarie@gmail.com>
2024-03-13 01:58:00 +0100

    Implemented AddToChest
    generateItem set to placeholder Stone Block x10

--- Common/BiomeExtraction.cs
+++ Common/BiomeExtraction.cs
@@ -1,19 +1,39 @@
-using System.Collections.Generic;
+using System;
+using Terraria;
+using Terraria.ID;
 
-namespace BiomeExtractorsMod
+namespace BiomeExtractorsMod.Common
 {
     public static class BiomeExtraction
     {
-        private static List<int> ExtractorCooldownTable = [];
-
-        internal static void PopulateExtractionSpeedTable()
+        internal static bool AddToChest(Item newItem, Chest chest)
         {
-            //TODO populate
+            int starting = newItem.stack;
+            for (int inventoryIndex = 0; inventoryIndex < Chest.maxItems; inventoryIndex++)
+            {
+                Item item = chest.item[inventoryIndex];
+                if (item.type == ItemID.None)
+                {
+                    item.SetDefaults(newItem.type);
+                    newItem.stack -= item.stack;
+                }
+                if (item.type == newItem.type) {
+                    int diff = item.maxStack - item.stack;
+                    int transfer = Math.Min(diff, newItem.stack);
+                    newItem.stack -= transfer;
+                    item.stack += transfer;
+                }
+                if(newItem.stack < 1) return true;
+            }
+            return starting != newItem.stack;
         }
 
-        public static int GetExtractionSpeed(int v)
+            internal static Item generateItem()
         {
-            return ExtractorCooldownTable[v];
+            Item item = new Item();
+            item.SetDefaults(ItemID.StoneBlock);
+            item.stack = 1;
+            return item;
         }
     }
 }
