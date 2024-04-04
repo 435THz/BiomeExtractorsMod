39483e8f8abcd402d5670af90b7f110c0e30afe8
MistressNebula <delbovemarie@gmail.com>
2024-04-04 20:01:01 +0200

    Fixed Extractor tiers

--- Content/TileEntities/BiomeExtractorEntEthereal.cs
+++ Content/TileEntities/BiomeExtractorEntEthereal.cs
@@ -6,7 +6,7 @@ namespace BiomeExtractorsMod.Content.TileEntities
 {
     public class BiomeExtractorEntEthereal : BiomeExtractorEnt
     {
-        public override int Tier { get => (int)EnumTiers.BASIC; }
+        public override int Tier { get => (int)EnumTiers.ETHEREAL; }
         public override int ExtractionRate { get => ModContent.GetInstance<ExtractorConfig>().Tier7ExtractorRate; }
         public override int ExtractionChance { get => ModContent.GetInstance<ExtractorConfig>().Tier7ExtractorChance; }
         protected override int TileType => ModContent.TileType<BiomeExtractorTileEthereal>();
--- Content/TileEntities/BiomeExtractorEntSteampunk.cs
+++ Content/TileEntities/BiomeExtractorEntSteampunk.cs
@@ -6,7 +6,7 @@ namespace BiomeExtractorsMod.Content.TileEntities
 {
     public class BiomeExtractorEntSteampunk : BiomeExtractorEnt
     {
-        public override int Tier { get => (int)EnumTiers.BASIC; }
+        public override int Tier { get => (int)EnumTiers.STEAMPUNK; }
         public override int ExtractionRate { get => ModContent.GetInstance<ExtractorConfig>().Tier4ExtractorRate; }
         public override int ExtractionChance { get => ModContent.GetInstance<ExtractorConfig>().Tier4ExtractorChance; }
         protected override int TileType => ModContent.TileType<BiomeExtractorTileSteampunk>();
