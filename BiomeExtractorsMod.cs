2635d283497f5a40fea2375c0f07d3815eeb2dab
MistressNebula <delbovemarie@gmail.com>
2024-04-04 19:59:56 +0200

    Added Magic Storage weak dependency

--- BiomeExtractorsMod.cs
+++ BiomeExtractorsMod.cs
@@ -4,5 +4,11 @@ namespace BiomeExtractorsMod
 {
     public class BiomeExtractorsMod : Mod
 	{
+        static internal bool MS_loaded;
+
+        public override void Load()
+        {
+            MS_loaded = ModLoader.HasMod("MagicStorage");
+        }
     }
 }
