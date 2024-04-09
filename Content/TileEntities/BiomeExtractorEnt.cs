09dc191490c61c623a96b65341df3864436a4de1
MistressNebula <delbovemarie@gmail.com>
2024-04-09 17:40:16 +0200

    Removed unnecessary using

--- Content/TileEntities/BiomeExtractorEnt.cs
+++ Content/TileEntities/BiomeExtractorEnt.cs
@@ -3,7 +3,6 @@ using BiomeExtractorsMod.Common.Systems;
 using BiomeExtractorsMod.Content.Tiles;
 using BiomeExtractorsMod.CrossMod;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using Terraria;
