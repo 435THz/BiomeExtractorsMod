15446d503b9e7f3d4bfbe285ef851eaa0f2c11f7
MistressNebula <delbovemarie@gmail.com>
2024-06-15 15:34:04 +0200

    Document BiomeExtractorItem

--- Content/Items/BiomeExtractorItem.cs
+++ Content/Items/BiomeExtractorItem.cs
@@ -2,9 +2,18 @@
 
 namespace BiomeExtractorsMod.Content.Items
 {
+    /// <summary>
+    /// The core Item class implemented by all BiomeExtractors.
+    /// </summary>
     abstract class BiomeExtractorItem : ModItem
     {
+        /// <summary>
+        /// Returns the id of the BiomeExtractorTile this Item is bound to.
+        /// </summary>
         protected abstract int TileId { get; }
+        /// <summary>
+        /// Returns the style index of the tile this Item creates.
+        /// </summary>
         protected virtual int TileStyle => 0;
 
         public override void SetStaticDefaults()
