e5f3f833004fce307266b37f921e1c7aa59baadb
MistressNebula <delbovemarie@gmail.com>
2024-06-27 20:26:11 +0200

    Lunar and Ethereal tiers emit light

--- Content/Tiles/BiomeExtractorTileEthereal.cs
+++ Content/Tiles/BiomeExtractorTileEthereal.cs
@@ -11,9 +11,28 @@ namespace BiomeExtractorsMod.Content.Tiles
 
         protected override BiomeExtractorEnt TileEntity => ModContent.GetInstance<BiomeExtractorEntEthereal>();
 
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Main.tileLighted[Type] = true;
+        }
         protected override void CreateMapEntries()
         {
             AddMapEntry(new(213, 20, 201), MapEntryName);
+        }
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            bool found = TileUtils.TryGetTileEntityAs(i, j, out BiomeExtractorEnt entity);
+            if (!found || !entity.Active)
+            {
+                r = 0.095f;
+                g = 0.075f;
+                b = 0.100f;
+                return;
+            }
+            r = 0.95f;
+            g = 0.75f;
+            b = 1.00f;
         }
         protected override int ItemType(Tile tile)
         {
--- Content/Tiles/BiomeExtractorTileLunar.cs
+++ Content/Tiles/BiomeExtractorTileLunar.cs
@@ -11,9 +11,28 @@ namespace BiomeExtractorsMod.Content.Tiles
 
         protected override BiomeExtractorEnt TileEntity => ModContent.GetInstance<BiomeExtractorEntLunar>();
 
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Main.tileLighted[Type] = true;
+        }
         protected override void CreateMapEntries()
         {
             AddMapEntry(new(69, 167, 119), MapEntryName);
+        }
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            bool found = TileUtils.TryGetTileEntityAs(i, j, out BiomeExtractorEnt entity);
+            if (!found || !entity.Active)
+            {
+                r = 0.100f;
+                g = 0.100f;
+                b = 0.075f;
+                return;
+            }
+            r = 1.00f;
+            g = 1.00f;
+            b = 0.75f;
         }
         protected override int ItemType(Tile tile)
         {
