2769c85c320293271cfe5eaef5cb38d3f444168a
435THz <delbovemarie@gmail.com>
2026-05-09 12:12:01 +0200

    Add extraction analyzer with placeholder sprite

---
+++ Content/Items/ExtractionAnalyzer.cs
@@ -0,0 +1,58 @@
+using System;
+using BiomeExtractorsMod.Common.Database;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using BiomeExtractorsMod.Common.UI;
+using Terraria.ID;
+using System.Collections.Generic;
+
+namespace BiomeExtractorsMod.Content.Items
+{
+    public class ExtractionAnalyzer : BiomeScanner
+    {
+        
+        public override bool? UseItem(Player player)
+        {
+            if (player.whoAmI == Main.myPlayer)
+            {
+                if (player.altFunctionUse != 2)
+                {
+                    BiomeExtractionSystem.ExtractionTier tier = CurrentTier ??
+                        throw new IndexOutOfRangeException("Could not find a valid registered tier");
+
+                    Point16 pCenter = new((int)((player.position.X + player.width * 0.5) / 16.0),
+                                           (int)((player.position.Y + player.height * 0.5) / 16.0));
+                    ModContent.GetInstance<UISystem>().OpenAnalyzerScannerInterface(pCenter, tier);
+                    return true;
+                }
+            }
+            return base.UseItem(player);
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> lines)
+        {
+            BiomeExtractionSystem.ExtractionTier tier = CurrentTier;
+
+            int index = lines.FindIndex(static line => line.Mod == "Terraria" && line.Name == "Tooltip6");
+            if (index >= 0)
+            {
+                if (tier is not null)
+                    lines[index].Text += $" {tier.Name}";
+                else
+                    lines[index].Text += $" None";
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<BiomeScanner>())
+                .AddIngredient(ItemID.BlackLens, 2)
+                .AddIngredient(ItemID.MeteoriteBar, 5)
+                .AddTile(TileID.TinkerersWorkbench)
+                .Register();
+        }
+    }
+    
+}
